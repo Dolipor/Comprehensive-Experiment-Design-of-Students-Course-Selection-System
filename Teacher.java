6afb3e864419757b91ddece38ce18bd50278a38d
Dolipor <57742924+Dolipor@users.noreply.github.com>
2019-12-08 13:09:59 +0800

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,11 @@
+package course;
+
+public class Teacher extends Personnel {
+	Subject subject;
+	public String toString(){
+		return "老师编号："+id+",老师："+name+",性别："+sex;
+	}
+	public Teacher(String id, String name, String sex) {
+		super(id, name, sex);
+	}
+}
